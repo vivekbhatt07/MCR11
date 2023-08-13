43a547e9abdc8331043eead62b7415cc11853109
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-08-13 14:54:28 +0530

    fix: year

--- src/Components/Filter/index.jsx
+++ src/Components/Filter/index.jsx
@@ -3,6 +3,11 @@ import { useData } from "../../Context";
 
 const Filter = () => {
   const { state, dispatch } = useData();
+  const yearList = state.moviesList.reduce((list, currentMovie) => {
+    return list.includes(currentMovie.year)
+      ? [...list]
+      : [...list, currentMovie.year];
+  }, []);
 
   return (
     <div className="flex gap-8">
@@ -33,14 +38,9 @@ const Filter = () => {
         }}
       >
         <option value="">Release Year</option>
-        <option value="1991">1991</option>
-        <option value="1992">1922</option>
-        <option value="1994">1994</option>
-        <option value="1999">1999</option>
-        <option value="2001">2001</option>
-        <option value="2003">2003</option>
-        <option value="2008">2008</option>
-        <option value="2010">2010</option>
+        {yearList.map((currentYear) => {
+          return <option value={currentYear}>{currentYear}</option>;
+        })}
       </select>
 
       <select
